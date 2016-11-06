2ee2598a483b8ad38323ca0429df7b07c6a35b4b
Quico Orozco <esi@plumbaria.com>
2016-11-06 20:01:17 +0100

    Terminado Ejercicio 3_19

--- app/src/main/java/com/plumbaria/listviewactionbar/MainActivity.java
+++ app/src/main/java/com/plumbaria/listviewactionbar/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayShowHomeEnabled(true);
         inicializarAdaptador();
         setListAdapter(adapter);
     }
